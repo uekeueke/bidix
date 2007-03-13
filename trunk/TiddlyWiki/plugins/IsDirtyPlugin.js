6eb4e2c81aa8acb11d9fc63c5669e870ba6bb314
bidix91 <bidix91@61cb509d-6a27-0410-b29c-9322d2deb753>
2007-03-13 21:47:31 +0000

    IsDirtyPlugin V 1.0.0

--- trunk/TiddlyWiki/plugins/IsDirtyPlugin.js
+++ trunk/TiddlyWiki/plugins/IsDirtyPlugin.js
@@ -1,19 +1,29 @@
 /***
 |''Name:''|IsDirtyPlugin|
 |''Description:''|When the TiddlyWiki needs to be saved the tiddler named IsDirty contains ' * ' else it is empty. IsDirty tiddler is also appended in front of the browser page title.<br>Hint: Put it in front of your SiteTitle in your PageTemplate or in your MainMenu as an indicator.<br>For now IsDirty: <<tiddler IsDirty>>|
-|''Version:''|0.1.0|
-|''Date:''|Mar 11, 2007|
+|''Version:''|1.0.0|
+|''Date:''|Mar 13, 2007|
 |''Source:''|http://tiddlywiki.bidix.info/#IsDirtyPlugin|
 |''Author:''|BidiX (BidiX (at) bidix (dot) info)|
 |''License:''|[[BSD open source license|http://tiddlywiki.bidix.info/#%5B%5BBSD%20open%20source%20license%5D%5D ]]|
 |''~CoreVersion:''|2.2.0|
 ***/
 //{{{
+version.extensions.IsDirtyPlugin = {
+	major: 1, minor: 0, revision: 0, 
+	date: new Date("Mar 13, 2007"),
+	source: 'http://tiddlywiki.bidix.info/#IsDirtyPlugin',
+	author: 'BidiX (BidiX (at) bidix (dot) info',
+	license: '[[BSD open source license|http://tiddlywiki.bidix.info/#%5B%5BBSD%20open%20source%20license%5D%5D]]',
+	coreVersion: '2.2.0'
+};
+
 if (!window.bidix) window.bidix = {}; // bidix namespace
 if (!bidix.core) bidix.core = {};
 bidix.setDirty = TiddlyWiki.prototype.setDirty;
 bidix.initIsDirty = function()
 {
+	var tags = "excludeLists excludeSearch";
 	var tiddler = store.getTiddler("IsDirty");
 	if (!tiddler) {
 		tiddler = new Tiddler("IsDirty");
@@ -27,7 +37,6 @@ bidix.initIsDirty = function()
 
 TiddlyWiki.prototype.setDirty = function(dirty)
 {
-	var tags = "excludeLists excludeSearch";
 	var indic = " * ";
 	var oldDirty = this.isDirty ();
 	bidix.setDirty.apply(this,arguments);
